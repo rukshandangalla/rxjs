7fc25089aee84862e2d681be7670927fbeb12cf0
Rukshan Dangalla <rukshand@lbfinance.lk>
2019-11-10 08:50:11 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'rxjs-fundamentals';
+export class AppComponent implements OnInit {
+
+  constructor(private api: ApiService) { }
+
+  ngOnInit(): void {
+    this.api.getProducts().subscribe(
+      data => {
+        console.log(data);
+      }
+    );
+  }
 }
